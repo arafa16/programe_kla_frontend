28676b75d1df70f4e83afa2c67a38b58ca2d6580
ara fa adri <arafa.sco@gmail.com>
2025-05-15 12:48:31 +0700

    initial commit

---
+++ src/router/index.tsx
@@ -0,0 +1,42 @@
+import { useRoutes } from "react-router-dom";
+import Menu from "../layouts/SideMenu";
+import DashboardOverview1 from "../pages/DashboardOverview1";
+import MenuViewUser from "../pages/menu/menuViewUser";
+import Login from "../pages/auth/login";
+import Register from "../pages/auth/register";
+import ErrorPage from "../pages/ErrorPage";
+
+function Router() {
+  const routes = [
+    {
+      path: "/admin",
+      element: <Menu />,
+      children: [
+        {
+          path: "/admin/dashboard",
+          element: <DashboardOverview1 />,
+        }
+      ],
+    },
+    {
+      path: "/",
+      element: <MenuViewUser />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+    {
+      path: "/error-page",
+      element: <ErrorPage />,
+    },
+  ];
+
+  return useRoutes(routes);
+}
+
+export default Router;
---
+++ src/stores/sideMenuSlice.ts
@@ -0,0 +1,47 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "./store";
+import { icons } from "../base-components/Lucide";
+
+export interface Menu {
+  icon: keyof typeof icons;
+  title: string;
+  pathname?: string;
+  subMenu?: Menu[];
+  ignore?: boolean;
+}
+
+export interface SideMenuState {
+  menu: Array<Menu | string>;
+}
+
+const initialState: SideMenuState = {
+  menu: [
+    "START MENU",
+    {
+      icon: "Home",
+      title: "Dashboard",
+      subMenu: [
+        {
+          icon: "Activity",
+          pathname: "/",
+          title: "Overview 1",
+        },
+        {
+          icon: "Activity",
+          pathname: "/dashboard-overview-2",
+          title: "Overview 2",
+        },
+      ],
+    }
+  ],
+};
+
+export const sideMenuSlice = createSlice({
+  name: "sideMenu",
+  initialState,
+  reducers: {},
+});
+
+export const selectSideMenu = (state: RootState) => state.sideMenu.menu;
+
+export default sideMenuSlice.reducer;
