53cffedd76630f1f974f9ebbe8611fc2c48b754c
ara fa adri <arafa.sco@gmail.com>
2025-05-19 22:21:57 +0700

    build: make routes for visit every page

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,16 @@
 import { useRoutes } from "react-router-dom";
 import Menu from "../layouts/SideMenu";
-import DashboardOverview1 from "../pages/DashboardOverview1";
 import MenuViewPage from "../pages/menu/menuViewPage";
 import Login from "../pages/auth/login";
 import Register from "../pages/auth/register";
-import ErrorPage from "../pages/ErrorPage";
+import MenuAdminViewPage from "../pages/menuAdmin/menuAdminViewPage";
+import MenuAdminCreatePage from "../pages/menuAdmin/menuAdminCreatePage";
+import MenuAdminViewByIdPage from "../pages/menuAdmin/menuAdminViewByIdPage";
+import MenuAdminEditPage from "../pages/menuAdmin/menuAdminEditPage";
+import UserAdminViewPage from "../pages/user/userAdminViewPage";
+import UserAdminCreatePage from "../pages/user/userAdminCreatePage";
+import UserAdminViewByIdPage from "../pages/user/userAdminViewByIdPage";
+import UserAdminEditPage from "../pages/user/userAdminEditPage";
 
 function Router() {
   const routes = [
@@ -13,9 +19,37 @@ function Router() {
       element: <Menu />,
       children: [
         {
-          path: "/admin/dashboard",
-          element: <DashboardOverview1 />,
-        }
+          path: "/admin/menu",
+          element: <MenuAdminViewPage />,
+        },
+        {
+          path: "/admin/menu/create",
+          element: <MenuAdminCreatePage />,
+        },
+        {
+          path: "/admin/menu/view/:uuid",
+          element: <MenuAdminViewByIdPage />,
+        },
+        {
+          path: "/admin/menu/edit/:uuid",
+          element: <MenuAdminEditPage />,
+        },
+        {
+          path: "/admin/user",
+          element: <UserAdminViewPage />,
+        },
+        {
+          path: "/admin/user/create",
+          element: <UserAdminCreatePage />,
+        },
+        {
+          path: "/admin/user/view/:uuid",
+          element: <UserAdminViewByIdPage />,
+        },
+        {
+          path: "/admin/user/edit/:uuid",
+          element: <UserAdminEditPage />,
+        },
       ],
     },
     {
@@ -29,10 +63,6 @@ function Router() {
     {
       path: "/register",
       element: <Register />,
-    },
-    {
-      path: "/error-page",
-      element: <ErrorPage />,
     },
   ];
 
