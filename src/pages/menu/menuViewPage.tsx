fa06a4b55280c2d493e477d09fce2f1616b43c77
ara fa adri <arafa.sco@gmail.com>
2025-05-23 09:39:19 +0700

    fix: grid in menu page

--- src/pages/menu/menuViewPage.tsx
+++ src/pages/menu/menuViewPage.tsx
@@ -12,7 +12,7 @@ const menuViewPage = () => {
     <>
         <div className="px-5 py-5 h-screen overflow-auto">
         <div className="box relative border-transparent bg-blue-400 bg-opacity-20 dark:bg-darkmode-600 intro-y md:w-full min-h-full">
-          <div className="grid grid-cols-1 md:grid-cols-4 gap-y-16 gap-x-16 px-12 pt-24 md:pt-12 pb-48 md:pb-12 justify-items-center">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-y-16 gap-x-16 px-12 pt-24 md:pt-12 pb-48 md:pb-12 justify-items-center">
             {/* BEGIN: Menu Content */}
             {menuData && menuData.map((item:any) => (
               <div key={item.id} className="bg-white p-5 rounded-md shadow-lg grid grid-cols-1 min-h-[200px] w-[250px]">
@@ -38,9 +38,11 @@ const menuViewPage = () => {
             ))}
             {/* END: Menu Content */}
           </div>
-          {/* BEGIN: Footer Content */}
-          <div className="absolute bottom-0  dark:bg-darkmode-600 w-full">
-            <div className="box bg-white bg-opacity-80 border-transparent grid grid-cols-12 mt-5 p-5 my-5 mx-5">
+          
+        </div>
+        {/* BEGIN: Footer Content */}
+          <div className="dark:bg-darkmode-600 w-full">
+            <div className="box bg-white bg-opacity-80 border-transparent grid grid-cols-12 mt-5 p-5 my-5">
               <div className="grid items-end justify-center md:justify-end mx-4 col-span-12 sm:col-span-1 sm:col-start-10">
                 <div className="flex justify-center md:justify-end">
                   <img src={logo} alt="Logo" className="w-8 h-8 mb-1" />
@@ -50,11 +52,9 @@ const menuViewPage = () => {
                 <div className="text-[9pt] text-center md:text-start">2025.v1.0.0</div>
                 <div className="text-[9pt] text-center md:text-start">Created by Ara Fa Adri</div>
               </div>
-              
             </div>
           </div>
           {/* END: Footer Content */}
-        </div>
       </div>
     </>
   )
